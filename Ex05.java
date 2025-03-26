fe65c668042c49b1df82de7b0502550425c602c5
PhDetonhe <pedroh.detonhe@email.com>
2025-03-26 15:24:33 -0300

    criar codigo de emprestimo que pede o valor de  emprestimo, numero de parcelas e salario do solicitante, aprovando o emprestimo se o valor da parcela for até 30% do salario do solicitante

---
+++ Ex05.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Github
+ Ex05
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex05 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Double n1, n2, n3, salario30porcento, valorparcela;
+        System.out.print("Qual sera o valor do emprestimo? : ");
+        n1 = ler.nextDouble();
+        System.out.print("Em quantas parcelas sera pago? : ");
+        n2 = ler.nextDouble();
+        System.out.print("Qual o salario do solicitante? : ");
+        n3 = ler.nextDouble();
+        salario30porcento = 30/n3 * 100;
+        valorparcela = n1/n2;
+        if ( valorparcela <= salario30porcento ){
+            System.out.println("O emprestimo foi aprovado!");
+        }
+        else {
+            System.out.println("O emprestimo foi reprovado!");
+        }
+     
+    }
+}
