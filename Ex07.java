2331fc6377d8ce0a15415992e4cd0558b515044b
PhDetonhe <pedroh.detonhe@gmail.com>
2025-03-31 09:12:54 -0300

    Update Ex07.java

--- Ex07.java
+++ Ex07.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 public class Ex07 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Double n1, n2, n3;
+        Double valorVend, comissao, salarioBruto;
         System.out.print("Qual o total vendido durante o mes de abril? ");
         valorVend = ler.nextDouble();
-        n2 = 10/valorVend * 100;
+        comissao = valorVend * 0.10;
         if ( valorVend > 2000 ){
-            n3 = 1200 + n2;
-            System.out.println("A comissao recebida é de "+ n2 +". O salario bruto juntamente da comissao é igual a: "+ n3 );
+            salarioBruto = 1200 + comissao;
+            System.out.println("A comissao recebida é de "+ comissao +". O salario bruto juntamente da comissao é igual a: "+ salarioBruto );
         }
         else {
             System.out.println("O numero de vendas foi insuficiente. A condicao para ganho de comissao foi reprovada.");
