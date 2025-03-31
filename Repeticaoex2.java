cc0a3ee3e338ca3770328d3355b8dfc2944068ea
PhDetonhe <pedroh.detonhe@gmail.com>
2025-03-31 11:40:51 -0300

    Exemplo estrutura de repeticao

---
+++ Repeticaoex2.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Repetiçãoex
+Github
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Repeticaooex
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int n;
+		System.out.println("Digite um numero, maior que zero");
+		n = ler.nextInt();
+		
+		for(int i=0; i <= 15; i++ ) {
+		System.out.println("Numero: "+i);
+		}
+	}
+}
