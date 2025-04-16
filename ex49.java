3f503c2f0556e8e5cef2a2d701cb594f7cfec907
PhDetonhe <pedroh.detonhe@gmail.com>
2025-04-16 12:08:22 -0300

    algoritmo que le 2 vetores de 10 elementos e soma ambos, exibindo o resultado.

--- ex49.java
+++ ex49.java
@@ -1,41 +1,27 @@
-//**************************************************************************************************************
-//
-//ex49Array
-//
-//Desenvolva um algoritmo que leia 2 vetores de 10 elementos inteiros
-//cada. Em seguida, calcule a soma desses vetores, guarde o resultado em um terceiro vetor e escreva o resultado.
-//***********************************************************************************************************************//
+/**************************************************************************************************************
+49 Desenvolva um algoritmo que leia 2 vetores de 10 elementos inteiros
+cada. Em seguida, calcule a soma desses vetores, guarde o resultado em um terceiro vetor e escreva o resultado.
+***********************************************************************************************************************/
 import java.util.Scanner;
-
-public class ex49Array
-{
+public class ex49 {
 	public static void main(String[] args) {
-	    int TAMANHO = 10;
-		int [] num1 = new int[TAMANHO];
-        int [] num2 = new int[TAMANHO];
-        int [] somaarrays = new int[TAMANHO];
+		int[] array1 = new int[10];
+		int[] array2 = new int[10];
 		Scanner ler = new Scanner(System.in);
-		
-		System.out.print("Primeiro array:\n");
-
-		for (int i = 0; i < TAMANHO; i++){
-		    System.out.print("Insira o "+ (i+1) + "° número: ");
-		    num1[i] = ler.nextInt();
+		System.out.println("Primeiro vetor: ");
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Insira o "+(i+1)+"° número: ");
+			array1[i] = ler.nextInt();
 		}
-		
-		System.out.print("Segundo array:\n");
-
-        for (int j = 0; j < TAMANHO; j++){
-		    System.out.print("Insira o "+ (j+1) + "° número: ");
-		    num2[j] = ler.nextInt();
+		System.out.println("Segundo vetor: ");
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Insira o "+(i+1)+"° número: ");
+			array2[i] = ler.nextInt();
 		}
-
-		int total = 0;
-		for (int i = 0; i < TAMANHO; i++) {
-		    total += num1[i] + num2[i];
+		int somaTotal = 0;
+		for (int i = 0; i < 10; i++) {
+			somaTotal += array1[i] + array2[i];
 		}
-		somaarrays[0] = total; 
-
-        System.out.println("A soma dos dois arrays é igual a: " + somaarrays[0]);
+		System.out.println("A soma dos dois arrays é igual a: " + somaTotal);
 	}
 }
